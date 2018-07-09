da8a3211421a49e745a3bf0a99b36358b855d9e3
sandipan100roy <39790970+sandipan100roy@users.noreply.github.com>
2018-07-09 18:04:51 +0530

    Update app.ts

--- lib/app.ts
+++ lib/app.ts
@@ -4,12 +4,14 @@ import { Request, Response } from "express";
 
 import * as mongoClient from "mongodb";
 
-import * as cryptoClient from "crypto";
-
-//import getusers from "./getusers";
 
 import { Getusers } from './getusers';
+import { Registeruser } from './registeruser';
+import { Loginuser } from './loginuser';
+
 const getusers = new Getusers();
+const registeruser = new Registeruser();
+const loginuser = new Loginuser();
 
 class App {
 
@@ -37,67 +39,7 @@ class App {
             dbo = db.db("userdetails");
       });
 
-
-    //Get All Users API
-    /*router.get('/', (req: Request, res: Response) => {
-            var query = {};
-            dbo.collection("userdetails").find(query).toArray(function(err, result) {
-                if (err) throw err;
-                 res.send(result);
-            });
-    });*/
-
-    //Login API
-    router.post('/loginuser', (req: Request, res: Response) => {
-            var query = { email: req.body.email, password: req.body.password};
-            var sha,generatedsession;
-            dbo.collection("userdetails").find(query).project({email: 1}).toArray(function(err,result) {
-                if (err) throw err;
-                if(result && result.length==1){
-                  
-                  //Create Session Id.
-                  sha = cryptoClient.createHash('sha256');
-                  sha.update(Math.random().toString());
-                  generatedsession=sha.digest('hex');
-
-                  //Store session in DB
-                  var sessionObj = {
-                    userid: result[0].email,
-                    sessionid: generatedsession
-                  }
-                  dbo.collection('usersessiondetails').insertOne(sessionObj, function(){
-                    if (err) throw err;
-                    console.log("User session inserted");
-                  });
-
-
-                  //Add Session Id To Response.
-                  result[0].sessionid = generatedsession;
-                  res.json(result)
-               }
-                else{
-                  res.send("User Doesn't Exists");
-                }
-            });
-    });
-
-    //Register API
-    router.post('/registeruser', (req: Request, res: Response) => {
-            const data = req.body;
-            var myObj = {
-              name: data.name,
-              email: data.email,
-              password: data.password,
-              phone: data.phone
-            };
-            dbo.collection("userdetails").insertOne(myObj, function(err, result){
-                if (err) throw err;
-                console.log("1 document inserted");
-                res.send("Response Code - "+result);
-            });
-            
-    });
-
+    
     //Logout API
     router.post('/logout', (req: Request, res: Response ) => {
         var deletequery = { userid: req.body.email };
@@ -110,6 +52,8 @@ class App {
 
     this.app.use('/', router);
     this.app.use('/getalluser', getusers.router);
+    this.app.use('/registeruser', registeruser.router);
+    this.app.use('/loginuser', loginuser.router);
     //module.exports = router;
 
   }
