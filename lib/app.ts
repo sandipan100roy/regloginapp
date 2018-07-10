3a9410ed5cc091cd3a3f616a976de10dba00d5a2
sandipan100roy <39790970+sandipan100roy@users.noreply.github.com>
2018-07-10 12:46:40 +0530

    Updated app.ts

--- lib/app.ts
+++ lib/app.ts
@@ -1,17 +1,15 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
-
-import * as mongoClient from "mongodb";
-
 
 import { Getusers } from './getusers';
 import { Registeruser } from './registeruser';
 import { Loginuser } from './loginuser';
+import { Logoutuser } from './logoutuser';
 
 const getusers = new Getusers();
 const registeruser = new Registeruser();
 const loginuser = new Loginuser();
+const logoutuser = new Logoutuser();
 
 class App {
 
@@ -31,31 +29,11 @@ class App {
   public routes(): void {
     const router = express.Router();
 
-    // Initialize MongoDB Connection Once
-    var url = "mongodb://localhost:27017/userdetails";
-    var dbo;
-    mongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            dbo = db.db("userdetails");
-      });
-
-    
-    //Logout API
-    router.post('/logout', (req: Request, res: Response ) => {
-        var deletequery = { userid: req.body.email };
-        dbo.collection("usersessiondetails").deleteOne(deletequery, function(err, result){
-          if (err) throw err;
-          console.log("Session deleted");
-          res.send("Response Code - "+result);
-        });
-    });
-
-    this.app.use('/', router);
+    //this.app.use('/', router);
     this.app.use('/getalluser', getusers.router);
     this.app.use('/registeruser', registeruser.router);
     this.app.use('/loginuser', loginuser.router);
-    //module.exports = router;
-
+    this.app.use('/logout', logoutuser.router);
   }
 
 }
