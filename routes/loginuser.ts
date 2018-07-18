7169934547e9cc6b3b09e938c4a63485771f10c9
sandipan100roy <39790970+sandipan100roy@users.noreply.github.com>
2018-07-18 18:19:59 +0530

    Code committed for the decryption of password!

--- routes/loginuser.ts
+++ routes/loginuser.ts
@@ -25,31 +25,57 @@ export class Loginuser {
 
 		//Login API
     	this.router.post('/', (req: Request, res: Response) => {
-            var query = { email: req.body.email, password: req.body.password};
+            var query = { email: req.body.email};
             var sha,generatedsession;
-            dbo.collection("userdetails").find(query).project({email: 1}).toArray(function(err,result) {
+
+            dbo.collection("userdetails").find(query).project({email: 1, password: 1}).toArray(function(err,result) {
                 if (err) throw err;
+
                 if(result && result.length==1){
-                  
-                  //Create Session Id.
-                  sha = cryptoClient.createHash('sha256');
-                  sha.update(Math.random().toString());
-                  generatedsession=sha.digest('hex');
 
-                  //Store session in DB
-                  var sessionObj = {
-                    userid: result[0].email,
-                    sessionid: generatedsession
+                  //Decrypt encryptedpassword Starts
+			
+                  var matched = false;
+                  //var decryptedPassword = this.passwordDecrypt(result[0].password);
+                  var myKey = cryptoClient.createDecipher('aes-128-cbc','mypassword');
+                  var myStr = myKey.update(result[0].password, 'hex', 'utf8');
+                  myStr += myKey.final('utf8');
+			
+                  //Decrypt encryptedpassword Ends
+
+                  if(myStr == req.body.password)
+                    matched = true;
+                  else
+                    matched = false;
+
+                  if(matched){
+
+                      //Create Session Id.
+                      sha = cryptoClient.createHash('sha256');
+                      sha.update(Math.random().toString());
+                      generatedsession=sha.digest('hex');
+
+                      //Store session in DB
+                      var sessionObj = {
+                        userid: result[0].email,
+                        sessionid: generatedsession
+                      }
+                      dbo.collection('usersessiondetails').insertOne(sessionObj, function(){
+                        if (err) throw err;
+                        console.log("User session inserted!!!");
+                      });
+
+
+                      //Add Session Id To Response.
+                      result[0].sessionid = generatedsession;
+                      res.json(result)
+
+                  }else{
+
+                      res.send("User Doesn't Exists");
+
                   }
-                  dbo.collection('usersessiondetails').insertOne(sessionObj, function(){
-                    if (err) throw err;
-                    console.log("User session inserted!!!");
-                  });
-
-
-                  //Add Session Id To Response.
-                  result[0].sessionid = generatedsession;
-                  res.json(result)
+                  
                }
                 else{
                   res.send("User Doesn't Exists");
@@ -58,4 +84,13 @@ export class Loginuser {
    		});
 
   	}
+
+    /*public passwordDecrypt(encryptedpassword: any) : any{
+
+      var myKey = cryptoClient.createDecipher('aes-128-cbc','mypassword');
+      var myStr = myKey.update(encryptedpassword, 'hex', 'utf8');
+      myStr += myKey.final('utf8');
+      return myStr;
+
+    }*/
 }
